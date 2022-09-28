937d174e4f475e22890c2c770d3f5aba17ade439
BryantD <durrell@innocence.com>
2022-09-27 19:10:36 -0700

    Forgot a dependency, added it to the config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="itch_jam",
     version="0.1",
     py_modules=["itch_jam"],
-    install_requires=["click", "cloup", "requests", "bs4", "tqdm", "dataset"],
+    install_requires=["click", "cloup", "requests", "bs4", "tqdm", "dataset", "rich"],
     entry_points="""
         [console_scripts]
         itch-jam=itch_jam:cli
